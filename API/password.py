130e23aacfd6679d4e8f8b382ad92449f9e6a4cc
puregame <mtthw.gougeon@gmail.com>
2015-10-03 22:01:00 -0400

    update password file

--- API/password.py
+++ API/password.py
@@ -1,5 +1,5 @@
-db_address	= ""
-db_user		= ""
-db_name		= ""
-db_port		= ""
-db_password	= ""
+db_address	= "Database Address" 	# postgres server address
+db_user		= "Database User" 		# name of postgres user
+db_name		= "Database Name" 		# name of database within PostGres
+db_port		= "5432"				# postgres port (default:5432)
+db_password	= ""					# database password

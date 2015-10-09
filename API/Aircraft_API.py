66b644c3af0aa8575dfe0d496a21705d6957eefd
puregame <mtthw.gougeon@gmail.com>
2015-10-09 14:24:11 -0400

    add mechanism to ignore aircraft if it's lat/lon or altitude was received less than 10 seconds ago

--- API/Aircraft_API.py
+++ API/Aircraft_API.py
@@ -1,84 +1,87 @@
-
-print "test"
-from bottle import Bottle, route, run, request
+from bottle import Bottle, route, run, request, static_file
 import json
-from Database_Connection import DB
-from APIException import *
-app = Bottle()
-
 import datetime
 
+
+# Internal py files
+from Database_Connection import DB
+from APIException import *
+from APILogic import *
+
+
+app = Bottle()
+checker = aircraftChecker()
 connection = DB()
 
+@app.route('/')
+def main_page():
+    return static_file('index.html', root='html/')
 
 @app.route('/hello/')
 def hello():
-	try:
-		latitude = request.query["lat"]
-		longitude = request.query["lon"]
-		print latitude
-		print longitude
-	except KeyError, e:
-		print "an error occured"
-		pass
+    return "hello world"
 
 @app.route('/addNote')
 def addNote():
-	query=request.query
-	try:
-		icao_hex = query["id"]
-		description = query["description"]
-		connection.updateAircraftDescription(icao_hex, description)
-	except ValueError, e:
-		return "request must include id and message"
-	return "worked"
+    query=request.query
+    try:
+        icao_hex = query["id"]
+        description = query["description"]
+        connection.updateAircraftDescription(icao_hex, description)
+    except ValueError, e:
+        return "request must include id and message"
+    return "worked"
 
 @app.route('/newMessage/')
 def newMessage():
-	query=request.query
-	data = {}
-	try: 
-		# try to parse request 
-		try:
-			data["id"] = query["id"]
-			data["signal"] = query["signal"]
-			data["time"] = query["time"]
-			data["station"] = query["station"]
-		except KeyError, e:
-			raise APIException("Request must include aircraft ID, signal signal_strength, time, and station")
-		try:
-			data["sqk"] = query["sqk"]
-			data["mode"] = query["mode"]
-		except KeyError, e:
-			data["sqk"] = 0
-			data["mode"] = "0"
-		try: # try to parse lat/lon
-			data["lat"] = query["lat"]
-			data["lon"] = query["lon"]
-		except KeyError, e:
-			data["lat"] = 0
-			data["lon"] = 0
-		try: # try to parse speed/heading
-			data["speed"] = query["speed"]
-			data["heading"] = query["heading"]
-		except Exception, e:
-			data["speed"] = 0
-			data["heading"] = 0
-		try: # try to parse fligt number
-			data["flight"] = query["flight"]
-		except KeyError, e:
-			data["flight"] = "0"
-		try: # try to parse fligt number
-			data["altitude"] = query["altitude"]
-		except KeyError, e:
-			data["altitude"] = 0
+    query=request.query
+    data = {}
+    try: 
+        # try to parse request 
+        try:
+            data["id"] = query["id"]
+            data["signal"] = query["signal"]
+            data["time"] = query["time"]
+            data["station"] = query["station"]
+        except KeyError, e:
+            raise APIException("Request must include aircraft ID, signal, time, and station")
+        try:
+            data["sqk"] = query["sqk"]
+            data["mode"] = query["mode"]
+        except KeyError, e:
+            data["sqk"] = 0
+            data["mode"] = "0"
+        try: # try to parse lat/lon
+            data["lat"] = query["lat"]
+            data["lon"] = query["lon"]
+        except KeyError, e:
+            data["lat"] = 0
+            data["lon"] = 0
+        try: # try to parse speed/heading
+            data["speed"] = query["speed"]
+            data["heading"] = query["heading"]
+        except Exception, e:
+            data["speed"] = 0
+            data["heading"] = 0
+        try: # try to parse fligt number
+            data["flight"] = query["flight"]
+        except KeyError, e:
+            data["flight"] = "0"
+        try: # try to parse fligt number
+            data["altitude"] = query["altitude"]
+        except KeyError, e:
+            data["altitude"] = 0
+    except APIException, e: 
+        return e.__str__()
+    except Exception, e:
+        print e
+        return "Error"
+    if checker.souldDBInsert(data):
+        # Upload to database
+        checker.performDBInsert(data)
+        connection.newUserMessage(data)
+        print "data uploaded to DB"
+    checker.printDebug()
+    return "Data accepted"
 
-		# Upload to database
-		connection.newUserMessage(data)
-	except APIException, e:
-		return e.__str__()
-	except Exception, e:
-		print e
-		return "ERROR"
-		
 run(app, host="localhost", port="8080")
--- API/Database_Connection.py
+++ API/Database_Connection.py
@@ -2,31 +2,31 @@ import psycopg2
 from psycopg2 import IntegrityError
 from password import * # file for storing database connection properties
 
-print db_address
-
 connection = psycopg2.connect(host = db_address, port = db_port, database=db_name, user=db_user, password=db_password)
 cursor = connection.cursor()
 
 class DB():
-	"""docstring for DB"""
-	def __init__(self):
-		self.connection = psycopg2.connect(host = db_address, port = db_port, database=db_name, user=db_user, password=db_password)
-		self.cursor = self.connection.cursor()
-	
-	def newUserMessage(self, data):
-		try:
-			self.cursor.execute("select flight_new_message(%(id)s::text, %(flight)s::text, %(altitude)s, %(speed)s::smallint, %(heading)s::smallint, %(signal)s::smallint, %(mode)s, %(lat)s::double precision, %(lon)s::double precision, %(sqk)s::smallint, %(station)s, %(time)s::timestamp with time zone)"
-						, data)
-			self.connection.commit()
-		except Exception,e :# should check for IntegrityError but that doesn't seem to work
-			# if an exception happens here we just need to rollback the current transaction and restart it
-			self.connection.rollback();
-			self.cursor.execute("select flight_new_message(%(id)s::text, %(flight)s::text, %(altitude)s, %(speed)s::smallint, %(heading)s::smallint, %(signal)s::smallint, %(mode)s, %(lat)s::double precision, %(lon)s::double precision, %(sqk)s::smallint, %(station)s, %(time)s::timestamp with time zone)"
-						, data)
-			self.connection.commit()
+    """docstring for DB"""
+    def __init__(self):
+        self.connection = psycopg2.connect(host = db_address, port = db_port, database=db_name, user=db_user, password=db_password)
+        self.cursor = self.connection.cursor()
+    
+    def newUserMessage(self, data):
+        print "test insert into DB with: "
+        print data
+        # try:
+        #     self.cursor.execute("select flight_new_message(%(id)s::text, %(flight)s::text, %(altitude)s, %(speed)s::smallint, %(heading)s::smallint, %(signal)s::smallint, %(mode)s, %(lat)s::double precision, %(lon)s::double precision, %(sqk)s::smallint, %(station)s, %(time)s::timestamp with time zone)"
+        #                 , data)
+        #     self.connection.commit()
+        # except Exception,e :# should check for IntegrityError but that doesn't seem to work
+        #     # if an exception happens here we just need to rollback the current transaction and restart it
+        #     self.connection.rollback();
+        #     self.cursor.execute("select flight_new_message(%(id)s::text, %(flight)s::text, %(altitude)s, %(speed)s::smallint, %(heading)s::smallint, %(signal)s::smallint, %(mode)s, %(lat)s::double precision, %(lon)s::double precision, %(sqk)s::smallint, %(station)s, %(time)s::timestamp with time zone)"
+        #                 , data)
+        #     self.connection.commit()
 
 
-	def updateAircraftDescription(self, icao_hex, description):
-		self.cursor.execute("UPDATE aircraft_spotted set user_notes=%(note)s where icao_hex=%(id)s", {'note':description, 'id':icao_hex})
-		self.connection.commit()
-		# cursor.execute("UPDATE aircraft_spotted set user_notes=%(note)s where icao_hex=%(id)s", {'note':description, 'id':icao_hex})
+    def updateAircraftDescription(self, icao_hex, description):
+        self.cursor.execute("UPDATE aircraft_spotted set user_notes=%(note)s where icao_hex=%(id)s", {'note':description, 'id':icao_hex})
+        self.connection.commit()
+        # cursor.execute("UPDATE aircraft_spotted set user_notes=%(note)s where icao_hex=%(id)s", {'note':description, 'id':icao_hex})

525a8be88a24f4558a7ca566e251501440738e4c
puregame <mtthw.gougeon@gmail.com>
2015-10-15 23:16:57 -0400

    updated password.py

---
+++ backend/password.py
@@ -0,0 +1,5 @@
+db_address  = "Database Address"    # postgres server address
+db_user     = "Database User"       # name of postgres user
+db_name     = "Database Name"       # name of database within PostGres
+db_port     = "5432"                # postgres port (default:5432)
+db_password = ""                    # database password
